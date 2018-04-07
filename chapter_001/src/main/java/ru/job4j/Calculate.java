1e6b4b1ae5b504ba2ba6fb512afdb579ff879d45
kajuga <msg2fedorov@gmail.com>
2018-04-07 18:53:30 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,16 @@
+
 package ru.job4j.calculate;
 
+/**
+* Class Класс для вычисления арифметических операций + - * / ^.
+* @author Aleksandr Fedorov (msg2fedorov@gmail.com)
+*/
 public class Calculate{
 
+/**
+* Вывод текстового сообщения в консоль
+* @param args - args.
+*/
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
