8cfe117f52e55f852c6fa17920b02a33150383cd
kajuga <msg2fedorov@gmail.com>
2018-04-08 18:29:05 +0300

    1.4. JUnit [#4122]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,18 +1,27 @@
-
-package ru.job4j.calculate;
-
-/**
-* Class Класс для вычисления арифметических операций + - * / ^.
-* @author Aleksandr Fedorov (msg2fedorov@gmail.com)
-*/
-public class Calculate{
+package ru.job4j;
 
 /**
-* Вывод текстового сообщения в консоль
-* @param args - args.
-*/
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
+ * Calculate.
+ *
+ * @author Aleksandr Fedorov (msg2fedorov@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Calculate {
+    /**
+     * Main.
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+    /**
+     * Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
 }
-
---
+++ chapter_001/src/test/java/ru/job4j/package-info.java
@@ -0,0 +1,9 @@
+/**
+* Package for CalculateTest task.
+*
+* @author Aleksandr Fedorov (msg2fedorov@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j;
+
