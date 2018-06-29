868dbdd5721aa102324b00d0d7bc6d57dc19eab6
kajuga <msg2fedorov@gmail.com>
2018-06-29 14:47:51 +0300

    2. Сортировка User с использованием Comparator

--- chapter_003/src/main/java/ru/job4j/sorting/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/sorting/SortUser.java
@@ -5,7 +5,24 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class SortUser {
-    public Set<User> sort(List <User> list) {
-        return new TreeSet<>(list);
+    public Set <User> sort(List <User> list) {
+        return new TreeSet <>(list);
     }
+
+
+    //TODO определить Comparator для метода Collections.sort и отсортировать List<User> по длине имени.
+    public List <User> sortNameLength(List <User> user) {
+
+        return null;
+    }
+
+    //TODO определить Comparator для метода Collections.sort и отсортировать List<User> по обоим полям,
+    // TODO сначала сортировка по имени в лексикографическом порядке, потом по возрасту.
+    public List <User> sortByAllFields(List <User> users) {
+
+
+        return null;
+    }
+
+
 }
