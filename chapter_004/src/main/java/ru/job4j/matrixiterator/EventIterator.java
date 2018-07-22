b3c62f0afb40b399d23afae2d8f9e7ea059cdb78
kajuga <msg2fedorov@gmail.com>
2018-07-22 21:24:00 +0300

    5.1.2. Создать итератор четные числа [#150] Доделал, протестировал приложенным тестом.

---
+++ chapter_004/src/main/java/ru/job4j/matrixiterator/EventIterator.java
@@ -0,0 +1,38 @@
+package ru.job4j.matrixiterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class EventIterator implements Iterator<Integer> {
+    private int[] value;
+    private int index = 0;
+
+    public EventIterator(int[] value) {
+        this.value = value;
+        movePosition();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return value.length > index;
+    }
+
+    @Override
+    public Integer next() {
+        try {
+            Integer result = value[index++];
+            movePosition();
+            return result;
+        }catch (IndexOutOfBoundsException e){
+            throw new NoSuchElementException();
+        }
+
+
+    }
+
+    private void movePosition(){
+        while (index < value.length && value[index] %2 != 0){
+            index++;
+        }
+    }
+}
