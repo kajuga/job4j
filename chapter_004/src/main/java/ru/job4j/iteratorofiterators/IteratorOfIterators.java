d755bb091c363e20d995fda05e5fb3e7f2cbbfc7
kajuga <msg2fedorov@gmail.com>
2018-07-25 17:50:05 +0300

    5.1.4. Создать convert(Iterator<Iterator>) Доделал, прикрутил тесты

--- chapter_004/src/main/java/ru/job4j/iteratorofiterators/IteratorOfIterators.java
+++ chapter_004/src/main/java/ru/job4j/iteratorofiterators/IteratorOfIterators.java
@@ -1,83 +1,55 @@
 package ru.job4j.iteratorofiterators;
 
+import java.util.ArrayList;
 import java.util.Iterator;
-
+import java.util.List;
+import java.util.NoSuchElementException;
 
 /**
- * Реализовать класс с методом Iterator<Integer> convert(Iterator<Iterator<Integer>> it).
+ * вот эту куевину нужно реализовать - класс Converter с методом convert
+ * 1. должен принимать итератор
  */
-
-public class IteratorOfIterators <T extends Number, I extends Iterator<T>> implements Iterator {
-
-    private final I[] iterators;
+class Converter implements Iterator<Integer> {
+    private Iterator[] iterators;
     private int index = 0;
 
-    public IteratorOfIterators(I[] iterators) {
-        this.iterators = iterators;
-    }
 
     @Override
     public boolean hasNext() {
-        return iterators[index].hasNext() || notDetectedLastIterator();
-    }
+        boolean hasNext = false;
 
-    private boolean notDetectedLastIterator() {
-        return (index + 1 < iterators.length);
+        while (iterators != null && index < iterators.length) {
+            hasNext = iterators[index].hasNext();
+            if (!hasNext) {
+                index++;
+            } else {
+                break;
+            }
+        }
+        return hasNext;
     }
 
     @Override
-    public T next() {
-        if (iterators[index].hasNext()) {
-            return iterators[index].next();
-        } else {
-            return iterators[++index].next();
+    public Integer next() {
+
+        try {
+            if (iterators[index].hasNext()) {
+                return (Integer) iterators[index].next();
+            } else {
+                return (Integer) iterators[++index].next();
+            }
+        } catch (IndexOutOfBoundsException e) {
+            throw new NoSuchElementException();
         }
+    }
+
+    Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
+        List<Iterator<Integer>> list = new ArrayList<>();
+        while (it.hasNext()) {
+            list.add(it.next());
+        }
+        Converter converter = new Converter();
+        converter.iterators = list.toArray(new Iterator[list.size()]);
+        return converter;
     }
 }
-
-    class Converter {
-
-
-        Iterator<Integer> convert(Iterator<Iterator<Integer>> it){
-
-            return null;
-        }
-    }
-
-
-    class SimpleIterator<T extends Number> implements Iterator {
-        private final T[] values;
-        private int index = 0;
-
-        public SimpleIterator(final T[] values) {
-            this.values = values;
-        }
-
-        @Override
-        public boolean hasNext() {
-            return index < values.length;
-        }
-
-        @Override
-        public T next() {
-            return values[index++];
-        }
-    }
-
-    class Client4 {
-        public static void main(String[] args) {
-            SimpleIterator <Integer> i1 = new SimpleIterator <>(new Integer[]{1, 2, 3});
-            SimpleIterator <Integer> i2 = new SimpleIterator <>(new Integer[]{1, 2, 3, 4});
-
-            IteratorOfIterators <Integer, Iterator <Integer>> itr = new IteratorOfIterators <>(new SimpleIterator[]{i1, i2});
-            while (itr.hasNext()) {
-                Integer i = itr.next();
-                System.out.println(i);
-            }
-        }
-    }
-
-
-
-
-
