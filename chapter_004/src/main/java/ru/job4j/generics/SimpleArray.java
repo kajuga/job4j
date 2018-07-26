5a597a6df37da383de2d94fe43ec5cc1e15acfb0
kajuga <msg2fedorov@gmail.com>
2018-07-26 20:54:59 +0300

    5.2.1. Реализовать SimpleArray<T>

---
+++ chapter_004/src/main/java/ru/job4j/generics/SimpleArray.java
@@ -0,0 +1,45 @@
+package ru.job4j.generics;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class SimpleArray<T> implements Iterable <T> {
+    private T[] model;
+    private int index;
+
+    public T add(T model) {
+        try {
+            return (T) (this.model[index++] = model);
+        } catch (IndexOutOfBoundsException e) {
+            throw new NoSuchElementException();
+        }
+    }
+
+    public T set(int index, T model) {
+        return (T) (this.model[index] = model);
+    }
+
+    public void delete(int index) {
+        System.arraycopy(model, index + 1, model, index, model.length - 1 - index);
+    }
+
+    public T get(int index) {
+        return (T) this.model[index];
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+
+            @Override
+            public boolean hasNext() {
+                return model.length > index;
+            }
+
+            @Override
+            public T next() {
+                return model[index++];
+            }
+        };
+    }
+}
