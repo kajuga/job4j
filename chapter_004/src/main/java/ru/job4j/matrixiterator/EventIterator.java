0aab5a7f460e00ca14c7d86313c193292203db51
kajuga <msg2fedorov@gmail.com>
2018-07-28 16:15:39 +0300

    5.1.2. Создать итератор четные числа [#62676] немного поправил код

--- chapter_004/src/main/java/ru/job4j/matrixiterator/EventIterator.java
+++ chapter_004/src/main/java/ru/job4j/matrixiterator/EventIterator.java
@@ -3,6 +3,12 @@ package ru.job4j.matrixiterator;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+/**
+ * EventIterator - итерируемся по четным числам массива.
+ * @author Fedorov Aleksander (msg2fedorov@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
 public class EventIterator implements Iterator<Integer> {
     private int[] value;
     private int index = 0;
@@ -12,18 +18,37 @@ public class EventIterator implements Iterator<Integer> {
         movePosition();
     }
 
+    /**
+     * Проверяю, есть ли четное число в диапазоне <текущий индекс - конец массива>;
+     * @return
+     */
     @Override
     public boolean hasNext() {
-        return value.length > index;
+        /*boolean result = false;
+        int counter = 0;
+        for(int i = index; i < value.length; i++){
+            if(value[i] % 2 == 0){
+                counter++;
+            }
+            if (counter > 0){
+                result = true;
+                break;
+            }
+        }*/
+        return index < value.length;
     }
 
+    /**
+     * Перемещаюсь по четным элементам массива
+     * @return
+     */
     @Override
     public Integer next() {
-        try {
+        if (hasNext()){
             Integer result = value[index++];
             movePosition();
             return result;
-        } catch (IndexOutOfBoundsException e) {
+        } else  {
             throw new NoSuchElementException();
         }
     }
