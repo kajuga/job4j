1084592de7623deb89ced89f72b66ac62ddf763f
kajuga <msg2fedorov@gmail.com>
2018-08-01 20:34:03 +0300

    5.3.1. Создать динамический список на базе массива Сделал, протестировал.

--- chapter_004/src/main/java/ru/job4j/list/DynamicArray.java
+++ chapter_004/src/main/java/ru/job4j/list/DynamicArray.java
@@ -1,41 +1,87 @@
 package ru.job4j.list;
 
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 
+/** Реализация динамически расширяющегося контейнера
+ * @author Fedorov Aleksander (msg2fedorov@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
 public class DynamicArray <E> implements Iterable<E> {
-    private Object[] container = new Object[10];
-    private int index;
+    private E[] values;
+    private int modCount;
 
-
-
-    // TODO: 01.08.18 добавить элемент - по умолчанию - в конец.
- public void add(E value){
-     this.container[index++] = value;
-
-
- }
-
-    // TODO: 01.08.18 реализовать динамику контейнера - расширение если нужно
-// public void
-
-
-
-    // TODO: 01.08.18 возвратить элемент по индексу
- public E get(int index){
-
-
-     return null;
- }
-
-
-
-
-
-    // TODO: 01.08.18 с этой херью в конце разобраться
-    @Override
-    public Iterator<E> iterator() {
-
-        return null;
+    DynamicArray(){
+        values = (E[]) new Object[0];
     }
 
+    /**
+     * Добавление элемента
+     * @param e
+     * @return
+     */
+    public boolean add (E e){
+        try{
+            E[] temp = values;
+            values = (E[]) new Object[temp.length + 1];
+            System.arraycopy(temp, 0, values, 0, temp.length);
+            values[values.length - 1] = e;
+            modCount++;
+            return true;
+        } catch (ClassCastException ex){
+            ex.printStackTrace();
+        }
+        return false;
+    }
+
+    /**
+     * Получение элемента по индексу
+     * @param index
+     * @return
+     */
+    public E get(int index) {
+        return values[index];
+    }
+
+    /**
+     * Получение размера контейнера
+     * @return
+     */
+    public int size(){
+        return values.length;
+    }
+
+    @Override
+    public Iterator<E> iterator(){
+        return new DynamicArrayIterator<>(values, modCount);
+    }
+
+    /**
+     * Итератор для контейнера
+     * @param <E>
+     */
+    private class DynamicArrayIterator<E> implements Iterator<E>{
+        private int index = 0;
+        private E[]values;
+        private int expectedModCount;
+
+        public DynamicArrayIterator(E[] values, int expectedModCount) {
+            this.expectedModCount = expectedModCount;
+            this.values = values;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return index < values.length;
+        }
+
+        @Override
+        public E next() {
+            if (expectedModCount != modCount){
+                throw new ConcurrentModificationException();
+            }
+            return values[index++];
+        }
+    }
 }
---
+++ chapter_004/src/test/java/ru/job4j/list/DynamicArrayTest.java
@@ -0,0 +1,55 @@
+package ru.job4j.list;
+
+import org.junit.Before;
+import org.junit.Test;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class DynamicArrayTest {
+    private DynamicArray<Integer> list;
+
+    @Before
+    public void BeforTest(){
+        list = new DynamicArray <>();
+        list.add(1);
+        list.add(5);
+        list.add(7);
+    }
+
+    @Test
+    public void whennAddedThreeSizeIsThree(){
+        assertThat(list.size(), is(3));
+    }
+
+    @Test
+    public void whenAddedThreeThenGetThird(){
+        assertThat(list.get(2), is(7) );
+    }
+
+    @Test
+    public void whenAddedThreeThenAddTenSizeIsThirteen(){
+        list.add(12);
+        list.add(12);
+        list.add(12);
+        list.add(12);
+        list.add(12);
+        list.add(12);
+        list.add(12);
+        list.add(12);
+        list.add(12);
+        list.add(12);
+        assertThat(list.size(), is(13));
+    }
+
+    @Test(expected = ConcurrentModificationException.class)
+    public void ConcurrentModificationExceptionTest(){
+        //DynamicArray arr = new DynamicArray();
+        Iterator<Integer> it = list.iterator();
+        it.next();
+        it.next();
+        list.add(2);
+        it.next();
+    }
+}
