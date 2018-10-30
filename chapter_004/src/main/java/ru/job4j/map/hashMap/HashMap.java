24130eb6c0c86755662ff0de65599cfa3c579d16
kajuga <msg2fedorov@gmail.com>
2018-10-30 15:46:12 +0300

    8. Реализовать собственную структуру данных - HashMap Доделал, тесты допилил, работает.

--- chapter_004/src/main/java/ru/job4j/map/hashMap/HashMap.java
+++ chapter_004/src/main/java/ru/job4j/map/hashMap/HashMap.java
@@ -8,58 +8,44 @@ import java.util.Objects;
 /**
  * HashMap realisaion.
  * @author Aleksandr Fedorov (msg2fedorov@gmail.com)
- * @param <K> key.
- * @param <V> value.
- * Node<K, V>[] hashTable - массив корзин для cписков нод <key, value>.
- * size - чеккер количества нодов списка в корзине.
- * threshold - параметр, контроллирующий заполненность таблицы-массива.
  */
 public class HashMap<K, V> implements Map<K, V> {
-    public static void main(String[] args) {
-        HashMap<String, String> strings = new HashMap <>();
-        strings.insert("a", "b");
-        System.out.println(strings.get("a"));
-    }
-
-
-    private Node<K, V>[] hashTable;
-    private int size = 0;
+    public Node<K, V>[] hashTable;
+    public int size = 0;
     private float threshold;
 
     /**
-     * Constructor - инициализирую масив, параметр threshold выставляется как 0.75 от его длины.
-     */
+     * Конструктор.
+      */
     public HashMap() {
         hashTable = new Node[16];
-        threshold = (float) (hashTable.length * 0.75);
+        threshold = hashTable.length * 0.75f;
     }
 
     /**
-     * Метод - вставка в мапу.
-     * @param key - key.
-     * @param value - value.
-     * сверка size с threshold, при возрастании количества нодов в корзине до соотношения
-     * ~0.75 к размеру хеш таблицы,  таблица увеличивается/перезаписывается в новый х2 массив методом arrayDoubling().
-     * @return boolean result
+     * Вставка элемента.
+     * Описаны два случая вставки: в прустую корзину  - simpleAdd(); занятую  - collisionProcessing().
+     * @param key - ключ.
+     * @param value - значение
+     * @return result вставки.
      */
     @Override
-    public boolean insert(K key, V value) {
-        //расширение массива при заполненности -> 0.75
+    public boolean insert(final K key, final V value) {
         if(size + 1 >= threshold) {
             threshold *= 2;
             arrayDoubling();
         }
-        //создается нода, присваевается номер бакета (index), куда ее поместить; если не занято, то simpleAdd() реализует добавление
+
         Node<K, V> newNode = new Node<>(key, value);
         int index = hash(key);
-        if(hashTable[index] == null) {
+        if (hashTable[index] == null) {
             return simpleAdd(index, newNode);
         }
-        //вытаскиваеся список нод из бакета[index], пробегаемся по списку; перезаписываем в случае keyExistButValueNew() или дописываем следом, при коллизии
-        //collisionProcessing()
+
         List<Node<K, V>> nodeList = hashTable[index].getNodes();
         for(Node<K, V> node : nodeList) {
-            if (keyExistButValueNew(node, newNode, value) || collisionProcessing(node, newNode, nodeList)) {
+            if (keyExistButValueNew(node, newNode, value) ||
+                    collisionProcessing(node, newNode, nodeList)) {
                 return true;
             }
         }
@@ -67,9 +53,9 @@ public class HashMap<K, V> implements Map<K, V> {
     }
 
     /**
-     * добавление ноды в незанятую корзину;
-     * @param index
-     * @param newNode
+     * Вставка ноды в пустую корзину hashTable.
+     * @param index номер корзины
+     * @param newNode новая нода.
      * @return
      */
     private boolean simpleAdd(int index, Node<K, V> newNode) {
@@ -80,14 +66,18 @@ public class HashMap<K, V> implements Map<K, V> {
     }
 
     /**
-     * Если корзина не пустая, проверяю список нод на равенство ключей: ключи равны, но value - нет - тогда "ок" на запись нового value поверх старого.
-     * @param nodeFromList
-     * @param newNode
-     * @param value
+     * Перезапись value ноды в случае совпадения ключа.
+     * @param nodeFromList  - нода со старым значением.
+     * @param newNode - новая.
+     * @param value - новое value.
      * @return
      */
-    private boolean keyExistButValueNew(Node<K, V> nodeFromList, Node<K, V> newNode, V value) {
-        if(newNode.getKey().equals(nodeFromList.getKey()) && !newNode.getValue().equals(nodeFromList.getValue())) {
+    private boolean keyExistButValueNew(
+            final Node<K, V> nodeFromList,
+            final Node<K, V> newNode,
+            final V value) {
+        if(newNode.getKey().equals(nodeFromList.getKey()) &&
+                !newNode.getValue().equals(nodeFromList.getValue())) {
             nodeFromList.setValue(value);
             return true;
         }
@@ -95,15 +85,19 @@ public class HashMap<K, V> implements Map<K, V> {
     }
 
     /**
-     * Проверка коллизии в списке из корзины.
-     * берем лист: если хэши равны, то и попадают объекты в одну корзинку списком, при этом должны !equals сами объекты ()
-     * @param nodeFromList
-     * @param newNode
-     * @param nodes
+     * Обработка  коллизии.
+     * Новая и уже лежашие в корзине ноды сравниваются по хэшкодам и value.
+     * @param nodeFromList  -  "старая".
+     * @param newNode - "новая"
+     * @param nodes - список нод в листе.
      * @return
      */
-    private boolean collisionProcessing(Node<K, V> nodeFromList, Node<K, V> newNode, List<Node<K, V>> nodes) {
-        if(newNode.hashCode() == nodeFromList.hashCode() && !Objects.equals(newNode.key, nodeFromList.key) && !Objects.equals(newNode.value, nodeFromList.value)) {
+    private boolean collisionProcessing(final Node<K, V> nodeFromList,
+                                        final Node<K, V> newNode,
+                                        final List<Node<K, V>> nodes) {
+        if(newNode.hashCode() == nodeFromList.hashCode() &&
+                !Objects.equals(newNode.key, nodeFromList.key) &&
+                !Objects.equals(newNode.value, nodeFromList.value)) {
             nodes.add(newNode);
             size++;
             return true;
@@ -134,13 +128,13 @@ public class HashMap<K, V> implements Map<K, V> {
      * @return
      */
     @Override
-    public boolean delete(K key) {
+    public boolean delete(final K key) {
         int index = hash(key);
             if(hashTable[index] == null) {
                 return false;
             }
             if (hashTable[index].getNodes().size() == 1) {
-                hashTable[index].getNodes().remove(0);
+                hashTable[index] = null;
                 return true;
         }
         List<Node<K, V>> nodeList = hashTable[index].getNodes();
@@ -159,9 +153,13 @@ public class HashMap<K, V> implements Map<K, V> {
      * @return
      */
     @Override
-    public V get(K key) {
+    public V get(final K key) {
         int index = hash(key);
-        if(index < hashTable.length && hashTable[index] != null) {
+        if(index < hashTable.length &&
+                hashTable[index] != null) {
+            if (hashTable[index].getNodes().size() == 1) {
+                return hashTable[index].getNodes().get(0).getValue();
+            }
             List<Node<K, V>> list = hashTable[index].getNodes();
             for (Node<K, V> node : list) {
                 if (key.equals(node.getKey())) {
@@ -169,82 +167,36 @@ public class HashMap<K, V> implements Map<K, V> {
                 }
             }
         }
-        return null;
+          return null;
+    }
+
+    public int size() {
+        return size;
     }
 
     /**
-     *
-     * @return номер корзины внутреннего массива.
+     * Дополнительный метод - добавил для теста, иначе никак у мня не получается в тестах hashTable.length вытащить.
+     * @return
      */
-    private int hash(K key) {
-        return hashCode() % hashTable.length;
+    public int hashTableLength() {
+        return hashTable.length;
     }
 
     /**
-     * Релизация нод в корзине таблицы. Каждая  - LinkedList.
-     * @param <K> key.
-     * @param <V> value.
-     *  hash - определяет методом % в какую корзину будет записана нода.
+     * Переопределил hash ключа для равномерного заполнения hashTable.
+     * @param key
+     * @return
      */
-    private class Node<K, V> {
-        private List<Node<K, V>> nodes;
-        private int hash;
-        private K key;
-        private V value;
-
-        private Node(K key, V value) {
-            this.key = key;
-            this.value = value;
-            nodes = new LinkedList <Node<K, V>>();
-        }
-
-        /**
-         * Возвращает из корзины список нод, в которой лежит нужная нода.
-         * @return
-         */
-        private List <Node <K, V>> getNodes() {
-            return nodes;
-        }
-
-
-
-        private K getKey() {
-            return key;
-        }
-
-        private V getValue() {
-            return value;
-        }
-
-        private void setValue(V value) {
-            this.value = value;
-        }
-
-        /**
-         * Переопределил hashCode для равномерного заполнения таблицы.
-         * @return
-         */
-        @Override
-        public int hashCode() {
-            hash = 31;
-            hash = hash * 17 + key.hashCode();
-            return hash;
-        }
-
-        /**
-         * и переопределил equals
-         * @param o
-         * @return
-         */
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Node <?, ?> node = (Node <?, ?>) o;
-            return key.equals(node.key);
-        }
+    private int hash(final K key) {
+        int hash = 31;
+        hash = hash * 17 + key.hashCode();
+        return hash % hashTable.length;
     }
 
+    /**
+     * Итератор.
+     * @return
+     */
     @Override
     public Iterator<V> iterator() {
         return new Iterator <V>() {
@@ -280,6 +232,77 @@ public class HashMap<K, V> implements Map<K, V> {
                 return subIterator.next().getValue();
             }
         };
+    }
+
+    /**
+     * Релизация нод в корзине таблицы. Каждая  - LinkedList.
+     * @param <K> key.
+     * @param <V> value.
+     *  hash - определяет методом % в какую корзину будет записана нода.
+     */
+    private class Node<K, V> {
+        private List<Node<K, V>> nodes;
+        private int hash;
+        private K key;
+        private V value;
+
+        private Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+            nodes = new LinkedList<Node<K, V>>();
+        }
+
+        /**
+         * Возвращает из корзины список нод, в которой лежит нужная нода.
+         * @return
+         */
+        private List <Node <K, V>> getNodes() {
+            return nodes;
+        }
+
+//        private int hash() {
+//            return hashCode() % hashTable.length;
+//        }
+
+        private K getKey() {
+            return key;
+        }
+
+        private V getValue() {
+            return value;
+        }
+
+        private void setValue(V value) {
+            this.value = value;
+        }
+
+        /**
+         * Переопределил hashCode для равномерного заполнения таблицы.
+         * @return
+         */
+        @Override
+        public int hashCode() {
+            hash = 31;
+            hash = hash * 17 + key.hashCode();
+            return hash;
+        }
+
+        /**
+         * и переопределил equals
+         * @param obj
+         * @return
+         */
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj instanceof Node) {
+                Node<K, V> node = (Node) obj;
+                return (Objects.equals(key, node.getKey()) &&
+                        Objects.equals(value, node.getValue()) ||
+                        Objects.equals(hash, node.hashCode()));
+            }
+            return false;
+        }
     }
 }
 
--- chapter_004/src/test/java/ru/job4j/map/hashMap/HashMapTest.java
+++ chapter_004/src/test/java/ru/job4j/map/hashMap/HashMapTest.java
@@ -1,41 +1,45 @@
 package ru.job4j.map.hashMap;
 
-import org.junit.Before;
 import org.junit.Test;
-
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class HashMapTest {
-    HashMap<String, Integer> stringHashMap;
 
-    @Before
-    public void beforeTest() {
-        stringHashMap = new HashMap <>();
-        stringHashMap.insert("bob", 15);
-        stringHashMap.insert("qwerty", 777);
-        stringHashMap.insert("terminator", 888);
+    @Test
+    public void whenAddKeySetThenGetValueByKey() {
+        HashMap<String, String> stringHashMap = new HashMap <>();
+        stringHashMap.insert("bob", "AAA");
+        assertThat(stringHashMap.get("bob"), is("AAA"));
+        stringHashMap.insert("bob", "BBB");
+        assertThat(stringHashMap.get("bob"), is("BBB"));
+        stringHashMap.insert("gaga", "qqq");
+        assertThat(stringHashMap.get("gaga"), is("qqq"));
+        assertThat(stringHashMap.delete("bob"), is(true));
     }
 
     @Test
-    public void whenGetValueByKey() {
-        assertThat(stringHashMap.get("bob"), is(15));
-        stringHashMap.insert("bob", 888);
-        assertThat(stringHashMap.get("bob"), is(888));
-//        assertThat(stringHashMap.get("qwerty"), is(777));
-
+    public void whenArrayIsFilledThenGrownTwice() {
+        HashMap<String, String> stringHashMap = new HashMap <>();
+        stringHashMap.insert("1", "a");
+        stringHashMap.insert("2", "b");
+        stringHashMap.insert("3", "c");
+        stringHashMap.insert("4", "d");
+        stringHashMap.insert("5", "e");
+        stringHashMap.insert("6", "g");
+        stringHashMap.insert("7", "h");
+        stringHashMap.insert("8", "j");
+        stringHashMap.insert("9", "k");
+        stringHashMap.insert("10", "l");
+        stringHashMap.insert("11", "m");
+        stringHashMap.insert("12", "n");
+        stringHashMap.insert("13", "o");
+        stringHashMap.insert("14", "p");
+        stringHashMap.insert("15", "LOL");
+        stringHashMap.insert("16", "r");
+        stringHashMap.insert("17", "s");
+        System.out.println(stringHashMap.size());
+        assertThat(stringHashMap.get("15"), is("LOL"));
+        assertThat(stringHashMap.hashTableLength(), is(32));
     }
-
-
-
-
-
-
-
 }
-
-/*
-
-        list.delete();
-        assertThat(list.get(0), is(2));
- */
