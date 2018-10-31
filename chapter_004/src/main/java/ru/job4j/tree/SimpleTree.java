416b0cabd32eb5e6959e94ed01ad321c38715c78
kajuga <msg2fedorov@gmail.com>
2018-10-31 20:12:23 +0300

    1. Создать элементарную структуру дерева

---
+++ chapter_004/src/main/java/ru/job4j/tree/SimpleTree.java
@@ -0,0 +1,21 @@
+package ru.job4j.tree;
+
+import java.util.Optional;
+
+public interface SimpleTree<E extends Comparable<E>> extends Iterable<E> {
+
+    /**
+     * Method add - add child for parent if parent present
+     * @param parent
+     * @param child
+     * @return
+     */
+    boolean add(E parent, E child);
+
+    /**
+     * Method findBy - find node by value
+     * @param value
+     * @return
+     */
+    Optional<Node<E>> findBy(E value);
+}
---
+++ chapter_004/src/main/java/ru/job4j/tree/Tree.java
@@ -0,0 +1,98 @@
+package ru.job4j.tree;
+
+import java.util.*;
+
+/**
+ * @author Aleksandr Fedorov (msg2fedorov@gmail.com)
+ */
+public class Tree<E extends Comparable<E>> implements SimpleTree<E> {
+    private Node<E> root;
+    private int modCount = 0;
+    private int size;
+
+    public Tree(E root) {
+        this.root = new Node<>(root);
+        this.size++;
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+
+    @Override
+    public boolean add(E parent, E child) {
+        boolean result = false;
+        Optional<Node<E>> parentResult = findBy(parent);
+        Optional<Node<E>> childResult = findBy(child);
+        if (parentResult.isPresent() && !childResult.isPresent()) {
+            Node<E> parentNode = parentResult.get();
+            Node<E> childNode = new Node<>(child);
+            parentNode.add(childNode);
+            result = true;
+            this.modCount++;
+            this.size++;
+        }
+        return result;
+    }
+
+    @Override
+    public Optional<Node<E>> findBy(E value) {
+        Optional<Node<E>> result = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> element = data.poll();
+            if (element.eqValue(value)) {
+                result = Optional.of(element);
+                break;
+            }
+            for (Node<E> child : element.leaves()) {
+                data.offer(child);
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new TreeIterator<>();
+    }
+
+    final class TreeIterator<E extends Comparable<E>> implements Iterator<E> {
+
+        Queue<Node<E>> data;
+
+        int expectedModCount = modCount;
+
+        @SuppressWarnings("unchecked")
+        public TreeIterator() {
+            this.data = new LinkedList<>();
+            this.data.offer((Node<E>) root);
+            Queue<Node<E>> temp = new LinkedList<>();
+            temp.offer((Node<E>) root);
+            while (!temp.isEmpty()) {
+                Node<E> element = temp.poll();
+                for (Node<E> child : element.leaves()) {
+                    this.data.offer(child);
+                    temp.offer(child);
+                }
+            }
+        }
+
+        @Override
+        public boolean hasNext() {
+            return !this.data.isEmpty();
+        }
+
+        @Override
+        public E next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            if (this.expectedModCount != modCount) {
+                throw new ConcurrentModificationException();
+            }
+            return this.data.poll().getValue();
+        }
+    }
+}
