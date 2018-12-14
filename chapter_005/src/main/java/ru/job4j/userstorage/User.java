e4d46d790e96287add6cf28a07524ccfbfa8e73e
kajuga <kajuga@mail.ru>
2018-12-14 18:03:05 +0300

    Code correction.

--- chapter_005/src/main/java/ru/job4j/userstorage/User.java
+++ chapter_005/src/main/java/ru/job4j/userstorage/User.java
@@ -1,5 +1,7 @@
 package ru.job4j.userstorage;
 
+import java.util.Objects;
+
 /**
  * Class User
  */
@@ -13,17 +15,38 @@ public class User {
     }
 
     public int getId() {
-        return this.id;
+        return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public int getAmount() {
-        return this.amount;
+        return amount;
     }
+
     public void setAmount(int amount) {
         this.amount = amount;
     }
 
+    /**
+     * По id шнику произвожу сравнивнение юзеров.
+     * @param o
+     * @return
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return id == user.id;
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
+
+
--- chapter_005/src/main/java/ru/job4j/userstorage/UserStorage.java
+++ chapter_005/src/main/java/ru/job4j/userstorage/UserStorage.java
@@ -5,12 +5,18 @@ import net.jcip.annotations.ThreadSafe;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * @author Aleksandr Fedorov(msg2fedorov@gmzil.com).
+ * User storage with money transfer feature realisation.
+ */
 @ThreadSafe
 public class UserStorage {
-    private volatile Map<Integer, User> users = new HashMap<>();
+    private volatile Map <Integer, User> users = new HashMap <>();
 
     /**
-     * Addind user into storage
+     * add any user'
+      * @param user - added user
+     * @return
      */
     public synchronized boolean add(User user) {
         if (!userCheck(user)) {
@@ -22,65 +28,73 @@ public class UserStorage {
     }
 
     /**
-     * Update user's amount
+     * edit any user.
+     * @param user - edited user.
+     * @return
      */
     public synchronized boolean update(User user) {
+        boolean result = false;
         if (userCheck(user)) {
-            this.users.put(user.getId(), new User(user.getId(), user.getAmount()));
-            return true;
-        } else {
-            return false;
+            users.put(user.getId(), user);
+            result = true;
         }
+        return result;
     }
 
     /**
-     * Delete user from storage
+     * delete user.
+     * @param user - deleting user.
+     * @return
      */
     public synchronized boolean delete(User user) {
+        boolean result = false;
         if (userCheck(user)) {
             users.remove(user.getId());
-            return true;
-        } else {
-            return false;
+            result = true;
         }
+        return result;
     }
 
     /**
-     * Transfer money
+     * transfer money from one user to other with cashcecking.
+     * @param fromId from
+     * @param toId  to
+     * @param cash how mutch
+     * @return boolean
      */
-    public synchronized boolean transfer(int fromId, int toId, int amount) {
+    public synchronized boolean transfer(Integer fromId, Integer toId, Integer cash) {
         boolean result = false;
         if (users.containsKey(fromId) && users.containsKey(toId)) {
-            if (users.get(fromId).getAmount() > amount) {
-                users.get(fromId).setAmount(users.get(fromId).getAmount() - amount);
-                users.get(toId).setAmount(users.get(toId).getAmount() + amount);
+            if (users.get(fromId).getAmount() > cash) {
+                users.get(fromId).setAmount(users.get(fromId).getAmount() - cash);
+                users.get(toId).setAmount(users.get(toId).getAmount() + cash);
                 result = true;
             }
-        } else {
-            result = false;
         }
         return result;
     }
 
     /**
-     * Проверка наличия usera в storage
+     * simple user checker.
+     * @param user user
+     * @return boolean
      */
-    public synchronized boolean userCheck(User user) {
-        boolean result  = false;
-        if (users.containsKey(user.getId())) {
-            result =  true;
-        }
-        return result;
+    public boolean userCheck(User user) {
+        return users.containsKey(user.getId());
     }
 
     /**
-     * Смотрю сколько денег на счету у юзера. с возвратом "0" некорректно, но что-нибудь придумаю.
+     * how much money has any user informer.
+     * @param user user for getting money info.
+     * @return money.
      */
-    synchronized int moneyObserver(User user) {
+    public int moneyObserver(User user) {
+        int result = 0;
         if (userCheck(user)) {
-            return users.get(user.getId()).getAmount();
-        } else {
-            return 0;
+            result = users.get(user.getId()).getAmount();
         }
+        return result;
     }
 }
+
+        
