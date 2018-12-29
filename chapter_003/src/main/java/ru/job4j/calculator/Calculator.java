a2ca5b2027ea2a4d41fcc7aa127133c2d0a0e608
kajuga <kajuga@mail.ru>
2018-12-29 18:40:10 +0300

    0.1 Ссылки на методы.

--- chapter_003/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_003/src/main/java/ru/job4j/calculator/Calculator.java
@@ -10,14 +10,13 @@ public class Calculator {
 
     public static void main(String[] args) {
         Calculator calc = new Calculator();
-        calc.multiple(0, 10, 2,(value, index) -> {
-            double result = value * index;
-            System.out.printf("Multiple %s * %s = %s %n", value, index, result);
-            return result;
-        }, result -> System.out.println(result));
+        calc.multiple(0, 10, 2, MathUtil::add,
+                result -> System.out.println(result));
     }
 
-    public void multiple(int start, int finish, int value, BiFunction <Integer, Integer, Double> op, Consumer <Double> media) {
+    public void multiple(int start, int finish, int value,
+                         BiFunction <Integer, Integer, Double> op,
+                         Consumer <Double> media) {
         for (int index = start; index != finish; index++) {
             media.accept(op.apply(value, index));
         }
--- chapter_003/src/main/java/ru/job4j/calculator/MathUtil.java
+++ chapter_003/src/main/java/ru/job4j/calculator/MathUtil.java
@@ -1,18 +1,11 @@
 package ru.job4j.calculator;
 
-import java.util.function.BiFunction;
-
 public class MathUtil {
-
     public static double add(int left, int second) {
         return left + second;
     }
+
     public static double div(int left, int second) {
         return left / second;
     }
-
-//    BiFunction<Integer, Integer, Double> op;
-
-
-
 }
