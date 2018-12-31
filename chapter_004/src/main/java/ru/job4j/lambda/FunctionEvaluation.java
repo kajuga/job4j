f738878d9021879fce28ae64123df155e67fa2a5
kajuga <kajuga@mail.ru>
2018-12-31 12:21:56 +0300

    1. Подсчет функции в диапазоне. привел к условию, добавил тесты.

--- chapter_004/src/main/java/ru/job4j/lambda/FunctionEvaluation.java
+++ chapter_004/src/main/java/ru/job4j/lambda/FunctionEvaluation.java
@@ -3,35 +3,18 @@ package ru.job4j.lambda;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.UnaryOperator;
-//import java.util.function.Function;
+
+/**
+ * @author Aleksandr Fedorov (msg2fedorov@gmail.com)
+ * Lambda simple realisation example
+ */
 
 public class FunctionEvaluation {
-
-
-    List<Integer> diapason(int start, int end, UnaryOperator<Integer> unar) {
-        List<Integer> result = new ArrayList<>();
+    List <Double> diapason(int start, int end, UnaryOperator <Double> unar) {
+        List <Double> result = new ArrayList <>();
         for (int index = start; index != end; index++) {
-            result.add(unar.apply(index));
+            result.add(unar.apply((double) index));
         }
         return result;
     }
-
-    public static void main(String[] args) {
-        FunctionEvaluation funcEva = new FunctionEvaluation();
-        funcEva.diapason(5, 8, (x) -> 2 * x + 1);
-    }
 }
-
-
-
-
-/*
-List<Double> diapason(int start, int end, Function<Double, Double> func);
-Линейной называется функция вида y=kx+by=kx+b, где kk и bb ­– любые числа (они называются коэффициентами).
-
-Квадратичной функцией называется функция вида y=ax2+bx+c, где a,b,c - числа, причем a≠0
-
-Функцию, заданную формулой  y=logax , называют логарифмической функцией с основанием  a .
-(a>0,a≠1) .
-
- */
--- chapter_004/src/test/java/ru/job4j/lambda/FunctionEvaluationTest.java
+++ chapter_004/src/test/java/ru/job4j/lambda/FunctionEvaluationTest.java
@@ -1,34 +1,27 @@
 package ru.job4j.lambda;
 
 import org.junit.Test;
-
 import java.util.Arrays;
 import java.util.List;
-
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class FunctionEvaluationTest {
+    private static FunctionEvaluation function = new FunctionEvaluation();
 
-//реализовал лист на Integerах, чтоб суть уяснить
     @Test
     public void whenLinearFunctionThenLinearResults() {
-        FunctionEvaluation function = new FunctionEvaluation();
-        List <Integer> result = function.diapason(5, 8, x -> 2 * x + 1);
-        List<Integer> expected = Arrays.asList(11, 13, 15);
+        List <Double> result = function.diapason(5, 8, x -> 2 * x + 1);
+        List<Double> expected = Arrays.asList(11D, 13D, 15D);
         assertThat(result, is(expected));
     }
+    @Test
+    public void whenSquareFunctionThenSquareResults() {
+        assertThat(function.diapason(1, 4, x -> Math.pow(x, 2) - x + 1), is(Arrays.asList(1D, 3D, 7D)));
+    }
 
-//todo
-//    @Test
-//    public void whenLinearFunctionThenLinearResults() {
-//        FunctionEvaluation function = new FunctionEvaluation();
-//        List <Double> result = function.diapason(5, 8, x -> 2 * x + 1);
-//        List<Double> expected = Arrays.asList(11D, 13D, 15D);
-//        assertThat(result, is(expected));
-//    }
-
-
-
-
+    @Test
+    public void whenLogarithmicFunctionThenLogarithmicResults() {
+        assertThat(function.diapason(1, 4, x -> Math.ceil(Math.exp(x) + x)), is(Arrays.asList(4D, 10D, 24D)));
+    }
 }
