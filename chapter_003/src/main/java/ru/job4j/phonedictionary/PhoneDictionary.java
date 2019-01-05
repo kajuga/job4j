6b2ca7b55c10784399823b06d8813986553afcab
kajuga <kajuga@mail.ru>
2019-01-05 14:17:09 +0300

    1. Stream API заменил методы в Телефонный справочник на базе ArrayList

--- chapter_003/src/main/java/ru/job4j/phonedictionary/PhoneDictionary.java
+++ chapter_003/src/main/java/ru/job4j/phonedictionary/PhoneDictionary.java
@@ -1,4 +1,32 @@
 package ru.job4j.phonedictionary;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * The phone dictionary.
+ * @author Aleksandr Fedorov (msg2fedorov@gmail.com)
+ */
 public class PhoneDictionary {
+    private List<Person> persons = new ArrayList<>();
+
+    /**
+     * Add a person in list.
+     * @param person - person.
+     */
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    /**
+     * find a person by any key with stream and other funny stuff.
+     * @param key
+     * @return
+     */
+    public List<Person> find(String key) {
+        return this.persons.stream()
+                .filter(p -> p.getName().contains(key) || p.getAddress().contains(key) || p.getPhone().contains(key) || p.getSurname().contains(key))
+                .collect(Collectors.toList());
+    }
 }
