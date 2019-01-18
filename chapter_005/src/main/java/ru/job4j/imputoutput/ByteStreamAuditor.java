7ab8d2b0b2e93ca126df5280f48868f4c04eae44
kajuga <kajuga@mail.ru>
2019-01-18 13:54:44 +0300

    1. Проверить байтовый поток Переписал, добавил тесты.

--- chapter_005/src/main/java/ru/job4j/imputoutput/ByteStreamAuditor.java
+++ chapter_005/src/main/java/ru/job4j/imputoutput/ByteStreamAuditor.java
@@ -5,36 +5,45 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
 
+/**
+ * @Autor Aleksandr Fedorov (msg2fedorov@gmail.com)
+ * Реализация проверки целого числа, считанного потоком на четность/нечетность
+ */
+
+
 public class ByteStreamAuditor {
 
     public boolean isNumber(InputStream in) throws IOException {
         boolean result = false;
 
-            try {
-                byte[] buffer = new byte[in.available()];                   //создаю байтовый массив, размером in.available (число байт в потоке)
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();   //создаю поток вывода
-                while (true) {
-                    int readBytesCount = in.read(buffer);                       //определяю маячок-проверялку наличия следующего байта во взодном потоке
-                    if (readBytesCount == -1) {                                 //если маячок срабатывает (возвращается -1) - break;
-                        break;
-                    }
-                    if (readBytesCount > 0) {                                   //пока в потоке есть следующий байт
-                        baos.write(buffer, 0, buffer.length);                //записываю в выходной поток некоторое число байтов, равное buffer.length
-                    }
+        try {
+            byte[] buffer = new byte[in.available()];                   //создаю байтовый массив, размером in.available (число байт в потоке)
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();   //создаю поток вывода
+            while (true) {
+                int readBytesCount = in.read(buffer);                       //определяю маячок-проверялку наличия следующего байта во взодном потоке
+                if (readBytesCount == -1) {                                 //если маячок срабатывает (возвращается -1) - break;
+                    break;
                 }
-                baos.flush();                                                   //допинываю остатки байтов в поток записи
-                baos.close();                                                   //закрываю поток записи
-                byte[] data = baos.toByteArray();                               //получаю байтовый массив, который нужно переконвертить каким-то образом в int
-                ByteBuffer bb = ByteBuffer.wrap(data);                          //Тээк, прочитал про волшебный Buffer из функционала java.nio
-                long l = bb.getLong();
-                if (l % 2 == 0) {
-                    result = true;
+                if (readBytesCount > 0) {                                   //пока в потоке есть следующий байт
+                    baos.write(buffer, 0, buffer.length);                //записываю в выходной поток число байтов, равное buffer.length
                 }
-
-            } catch (IOException e) {
-                e.printStackTrace();
             }
-return result;
+            baos.flush();                                                   //допинываю остатки байтов в поток записи
+            baos.close();                                                   //закрываю поток записи
+            byte[] data = baos.toByteArray();                               //получаю байтовый массив, который нужно переконвертить каким-то образом в число
+            ByteBuffer bb = ByteBuffer.wrap(data);                          //Тээк, прочитал про волшебный Buffer из функционала java.nio
+            long l = bb.getInt();
+            if (l % 2 == 0) {
+                result = true;
+            }
+//                if (byteArrayToInt(data) % 2 == 0) {                      //тут лажа - рассчитывает по чет/нечет количеству ячеек в байтовом массиве почему то.
+//                    result = true;
+//                }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
 
     }
 }
--- chapter_005/src/test/java/ru/job4j/imputoutput/ByteStreamAuditorTest.java
+++ chapter_005/src/test/java/ru/job4j/imputoutput/ByteStreamAuditorTest.java
@@ -1,24 +1,62 @@
 package ru.job4j.imputoutput;
 
+import org.junit.Test;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
 import static org.junit.Assert.*;
 
 public class ByteStreamAuditorTest {
+    private ByteStreamAuditor byteStreamAuditor = new ByteStreamAuditor();
+
+    /**
+     * Поток читает целое четное числож
+     */
+    @Test
+    public void whenInputStreamReadEvenThenIsNumberIsTrue() {
+        int value = 123456788;
+        byte[] bytes = ByteBuffer.allocate(4).putInt(value).array();
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+        try {
+            assertTrue(byteStreamAuditor.isNumber(byteArrayInputStream));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Поток считвывает целое нечетное число
+     */
+    @Test
+    public void whenInputStreamReadOddThenIsNumberIsTrue() {
+        int value = 123456789;
+        byte[] bytes = ByteBuffer.allocate(4).putInt(value).array();
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+        try {
+            assertFalse(byteStreamAuditor.isNumber(byteArrayInputStream));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Добавил проверку произвольного небольшого четного числа
+     */
+    @Test
+    public void whenInputStreamReadAnotherEvenThenIsNumberIsTrue() {
+        int value = 992;
+        byte[] bytes = ByteBuffer.allocate(4).putInt(value).array();
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+        try {
+            assertTrue(byteStreamAuditor.isNumber(byteArrayInputStream));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 
 
 
 }
-
-
-/*
-
-ByteBuffer bb = ByteBuffer.wrap(new byte[] {0, 0, 0, 0, 0, 0, 0, 4});
-long l = bb.getLong();
-
-System.out.println(l);
-
-Результат - 4
- */
-
-
-
